8ac2b37e498c32ddc87c7d7a0dc8f70276dd0d89
beurmuz <fallinta2@gmail.com>
2021-03-25 23:07:20 +0900

    2

---
+++ hello.java
@@ -0,0 +1,12 @@
+class hello {
+    public static void main(String[] args) {
+        System.out.println(10/3);
+        int a = 2; // 2
+        float b = 2.2F; 
+        double c = 2.2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
