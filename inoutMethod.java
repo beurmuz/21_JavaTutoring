10b1a45d5afe15def185ed2f3684d7558ae1f5f3
beurmuz <fallinta2@gmail.com>
2021-03-28 22:10:10 +0900

    two parameter

--- inoutMethod.java
+++ inoutMethod.java
@@ -2,7 +2,7 @@ public class inoutMethod {
 
     public static void repeatGo(int i,int j) {
         int num;
-        System.out.println(i +"에서 시작해서, " + j +"까지 반복합니다.");
+        System.out.println(i +"에서 시작해서, " + (j-1) +"까지 반복합니다.");
         for(num=i; num < j; num++) {
             System.out.println("현재 num의 값 : " + num);
         }
