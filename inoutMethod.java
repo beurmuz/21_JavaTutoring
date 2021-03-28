c733f93155020582c6dc024885937482c9068d8d
beurmuz <fallinta2@gmail.com>
2021-03-29 00:10:01 +0900

    return

--- inoutMethod.java
+++ inoutMethod.java
@@ -1,13 +1,15 @@
 public class inoutMethod {
 
-    public static void repeatGo(int i,int j) {
+    public static int repeatGo(int i,int j) {
         int num;
-        System.out.println(i +"에서 시작해서, " + (j-1) +"까지 반복합니다.");
+        int total = 0;
+        System.out.println(i +"에서 시작해서, " + (j-1) +"까지 합산합니다.");
         for(num=i; num < j; num++) {
-            System.out.println("현재 num의 값 : " + num);
+            total += num;
         }
+        return total;
     }
     public static void main(String[] args) {
-        repeatGo(0,6);
+        System.out.println(repeatGo(0,6));
     }
 }
