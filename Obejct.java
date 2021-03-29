50375696d1d7518508163db0ad14d77a6c08708a
beurmuz <fallinta2@gmail.com>
2021-03-30 00:27:04 +0900

    2 method program

--- Obejct.java
+++ Obejct.java
@@ -1,12 +1,18 @@
 public class Obejct {
     public static void sum(int i, int j) {
-        System.out.println(i+j);
+        System.out.println("sum : " + (i+j));
+    }
+    public static void avg(int i, int j) {
+        System.out.println("avg : " + ((i+j)/2));
     }
     public static void main(String[] args) {
         sum(2,1);
+        avg(2,1);
+
         sum(2,2);
-        sum(2,3);
+        avg(2,2);
         // ...
         sum(9,9);
+        avg(9,9);
     }
 }
