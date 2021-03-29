e2918833818ca62d7bf5e70fa41f83ded729dd48
beurmuz <fallinta2@gmail.com>
2021-03-30 00:20:30 +0900

    basic program

---
+++ Obejct.java
@@ -0,0 +1,9 @@
+public class Obejct {
+    public static void main(String[] args) {
+        System.out.println(2+1);
+        System.out.println(2+2);
+        System.out.println(2+3);
+        // ...
+        System.out.println(9+9);
+    }
+}
