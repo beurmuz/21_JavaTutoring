2f9f5803be5fa6c447c906ae0d99371fd3b4030e
beurmuz <fallinta2@gmail.com>
2021-03-31 22:02:09 +0900

    PI값 추가하기

--- Obejct.java
+++ Obejct.java
@@ -1,5 +1,6 @@
 class Calculator {
-    int a,b; // 전역 변수 선언
+    int a,b; 
+    static double PI = 3.14;
 
     public void setOprands(int a, int b) {
         this.a = a;
@@ -19,13 +20,9 @@ public class Obejct {
         public static void main(String[] args) {
 
         Calculator c1 = new Calculator();
-        c1.setOprands(2, 1);
-        c1.sum();
-        c1.avg();
+        System.out.println(c1.PI);
 
         Calculator c2 = new Calculator();
-        c2.setOprands(3, 5);
-        c2.sum();
-        c2.avg();
+        System.out.println(c2.PI);
     }
 }
