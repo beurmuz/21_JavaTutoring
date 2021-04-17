9228f7a2acc01a298fd685cdca11aaa11f0f249c
beurmuz <fallinta2@gmail.com>
2021-04-17 18:46:59 +0900

    abstract,overriding

--- abstractCode.java
+++ abstractCode.java
@@ -1,17 +1,23 @@
 public class abstractCode {
     public static void main(String[] args) {
-        A obj = new A();
+        B obj = new B();
+        System.out.println(obj.b());
     }
 }
 
+// 추상 클래스A 
 abstract class A {
-    public abstract int b();
-    // 내용이 있는 메소드는 abstract 키워드를 가질 수 없음 
-    public abstract int c() {
-        System.out.println("Hello");
-    }
-    // 추상 클래스 내에는 추상 메소드가 아닌 메소드가 존재할 수 있음
+    public abstract String b();
+    
     public void d() {
         System.out.println("world");
+    }
+}
+
+// 클래스A를 상속한 하위 클래스 만들기
+class B extends A {
+    public String b() {
+        String syn = "클래스A 사용하기";
+        return syn;
     }
 }
