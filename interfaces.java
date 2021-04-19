88031b81726163d7baaf1073abdb763501a3d90e
beurmuz <fallinta2@gmail.com>
2021-04-19 23:02:05 +0900

    calculatorConsumer

---
+++ interfaces.java
@@ -0,0 +1,13 @@
+interface I {
+    public void z();
+}
+
+class A implements I {
+    public void z() {}
+}
+
+public class interfaces {
+    public static void main(String[] args) {
+        
+    }
+}
